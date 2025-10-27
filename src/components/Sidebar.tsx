a34a9fd0fb51a6f605d93ceb5e3bd41e7c8058db
Cedrenou <ced2411@gmail.com>
2025-10-27 16:05:50 +0100

    add new page on sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import Link from "next/link";
-import { FaTachometerAlt, FaFileAlt, FaEnvelope, FaUserCircle } from "react-icons/fa";
+import { FaTachometerAlt, FaFileAlt, FaUserCircle } from "react-icons/fa";
 
 const links = [
   { href: "/", label: "Dashboard", icon: <FaTachometerAlt /> },
-  { href: "/factures", label: "Factures", icon: <FaFileAlt /> },
-  { href: "/emails", label: "Emails", icon: <FaEnvelope /> }
+  { href: "/renouvellement-annonces", label: "Renouvellement Annonces", icon: <FaFileAlt /> }
 ];
 
 const Sidebar = () => (
